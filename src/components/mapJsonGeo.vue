dd8234774d9a9e036e08826480beaa788ddb6367
delimond <gastondelimond@gmail.com>
2022-09-16 18:31:48 +0100

    map update

--- src/components/mapJsonGeo.vue
+++ src/components/mapJsonGeo.vue
@@ -1,115 +1,215 @@
 <template>
-    <div id="viewDiv"></div>
+  <div id="viewDiv" class="shadow">
+    <div id="baseMapDiv" class="h-[100px]"></div>
+  </div>
 </template>
 
-
 <script>
-import Map from "@arcgis/core/Map"
-import GeoJSONLayer from "@arcgis/core/layers/GeoJSONLayer"
-import MapView from "@arcgis/core/views/MapView"
+/* eslint-disable */
+import Map from "@arcgis/core/Map";
+import GeoJSONLayer from "@arcgis/core/layers/GeoJSONLayer";
+import MapView from "@arcgis/core/views/SceneView";
+import Search from "@arcgis/core/widgets/Search";
+import BasemapGallery from "@arcgis/core/widgets/BasemapGallery";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+import LayerList from "@arcgis/core/widgets/LayerList";
+import PopupTemplate from "@arcgis/core/PopupTemplate";
+import CustomContent from "@arcgis/core/popup/content/CustomContent";
+import Query from "@arcgis/core/rest/support/Query";
+import query from "@arcgis/core/rest/query";
+import StatisticDefinition from "@arcgis/core/rest/support/StatisticDefinition";
+import Intl from "@arcgis/core/intl";
+
+import TileLayer from "@arcgis/core/layers/TileLayer";
+import Measurement from "@arcgis/core/widgets/Measurement";
+import Legend from "@arcgis/core/widgets/Legend";
+
+import DictionaryRenderer from "@arcgis/core/renderers/DictionaryRenderer";
+import Expand from "@arcgis/core/widgets/Expand";
+import Graphic from "@arcgis/core/Graphic";
+
 export default {
-    name: "MapJsonGeo",
+  name: "MapJsonGeo",
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
+  props: {
+    url: String,
+  },
+  destroyed() {
+    this.$router.go(-1);
+    console.log("destroyed");
+  },
+  mounted() {
+    this.loadMap();
+  },
+  methods: {
+    loadMap() {
+      this.isLoading = true;
+      const url = this.url;
+      /*const template = {
+        title: "SNIA Cartographie agricole",
+        content:
+          "Estimation de recolte {mag} tonnes/mois {type} \n Endroit : {place}",
+        fieldInfos: [
+          {
+            fieldName: "time",
+            format: {
+              dateFormat: "short-date-short-time",
+            },
+          },
+        ],
+      };*/
+      const template = {
+        title: "SNIA Cartographie agricole",
+        content: [
+          {
+            type: "fields", // Autocasts as new FieldsContent()
+            // Autocasts as new FieldInfo[]
+            fieldInfos: [
+              {
+                fieldName: "Production",
+              },
+              {
+                fieldName: "Population",
+              },
+              {
+                fieldName: "Territoire",
+              },
+              {
+                fieldName: "Province",
+              },
+            ],
+          },
+        ],
+        expressionInfos: [
+          {
+            expression: "$features.properties.Production + tonnes/mois",
+          },
+          {
+            expression: "$features.properties.Population",
+          },
+          {
+            expression: "$features.properties.Territoire",
+          },
+          {
+            expression: "$features.properties.Province",
+          },
+        ],
+      };
+      const renderer = {
+        type: "simple",
+        symbol: {
+          type: "simple-marker",
+          style: "circle",
+          color: [250, 250, 250],
+          outline: {
+            color: [255, 255, 255, 0.5],
+            width: 0.5,
+          },
+        },
 
-    props: {
-        url:String
-    },
+        visualVariables: [
+          {
+            type: "color",
+            field: "Production",
+            stops: [
+              { value: 20, color: "red", size: "10px" },
+              { value: 35, color: "orange", size: "15px" },
+              { value: 50, color: "cyan", size: "20px" },
+              { value: 65, color: "lime", size: "25px" },
+              { value: 80, color: "green", size: "30px" },
+            ],
+          },
+        ],
+      };
+      const geojsonLayer = new GeoJSONLayer({
+        url: url,
+        copyright: "Analyse de données agricoles de la Rep. Dem. du Congo",
+        popupTemplate: template,
+        renderer: renderer,
+        orderBy: {
+          field: "Production",
+        },
+      });
+      const map = new Map({
+        basemap: "hybrid",
+        layers: [geojsonLayer],
+      });
+      const view = new MapView({
+        container: "viewDiv",
+        center: [22.95, -6.651],
+        zoom: 7,
+        map: map,
+      });
+      const basemapGallery = new BasemapGallery({
+        view: view,
+      });
 
-    mounted(){
-        this.loadMap();
-    },
-
-    methods: {
-        loadMap() {
-            // If GeoJSON files are not on the same domain as your website, a CORS enabled server
-            // or a proxy is required.
-            const url = this.url;
-
-            // Paste the url into a browser's address bar to download and view the attributes
-            // in the GeoJSON file. These attributes include:
-            // * mag - magnitude
-            // * type - earthquake or other event such as nuclear test
-            // * place - location of the event
-            // * time - the time of the event
-            // Use the Arcade Date() function to format time field into a human-readable format
-
-            const template = {
-                title: "Earthquake Info",
-                content: "Magnitude {mag} {type} hit {place} on {time}",
-                fieldInfos: [
-                    {
-                        fieldName: 'time',
-                        format: {
-                            dateFormat: 'short-date-short-time'
-                        }
-                    }
-                ]
+      const layerList = new LayerList({
+        view: view,
+        listItemCreatedFunction: (event) => {
+          const item = event.item;
+          if (item.layer.type != "group") {
+            // don't show legend twice
+            item.panel = {
+              content: basemapGallery,
+              open: false,
             };
+          }
+        },
+      });
 
-            const renderer = {
-                type: "simple",
-                field: "mag",
-                symbol: {
-                    type: "simple-marker",
-                    color: "orange",
-                    outline: {
-                        color: "white"
-                    }
-                },
-                visualVariables: [{
-                    type: "size",
-                    field: "mag",
-                    stops: [{
-                        value: 2.5,
-                        size: "4px"
-                    },
-                    {
-                        value: 8,
-                        size: "40px"
-                    }
-                    ]
-                }]
-            };
+      const search = new Search({
+        view: view,
+      });
+      view.ui.add(layerList, "bottom-right");
+      // places the search widget in the top right corner of the view
+      view.ui.add(search, "top-right");
 
-            const geojsonLayer = new GeoJSONLayer({
-                url: url,
-                copyright: "USGS Earthquakes",
-                popupTemplate: template,
-                renderer: renderer,
-                orderBy: {
-                    field: "mag"
-                }
-            });
-
-            const map = new Map({
-                basemap: "gray-vector",
-                layers: [geojsonLayer]
-            });
-
-            const view = new MapView({
-                container: "viewDiv",
-                center: [-168, 46],
-                zoom: 2,
-                map: map
-            });
-
-            view.when(()=>{
-                console.log("Map ready");
-            })
-        }
-    }
-}
+      view.when(() => {
+        this.isLoading = false;
+        console.log("Map ready");
+      });
+    },
+  },
+};
 </script>
 
-
 <style>
-    @import url("https://js.arcgis.com/4.24/esri/themes/light/main.css");
+@import url("https://js.arcgis.com/4.24/esri/themes/light/main.css");
 
-    html,
-    body,
-    #viewDiv {
-      padding: 0;
-      margin: 0;
-      height: 100%;
-      width: 100%;
-    }
+#viewDiv {
+  padding: 0;
+  margin: 0;
+  height: 100%;
+  width: 100%;
+}
+
+.esri-view:focus,
+.esri-view:focus {
+  border: none;
+}
+
+#titleDiv {
+  position: absolute;
+  height: 55px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  color: #f8f8f8;
+  background: #464646;
+  z-index: 1;
+}
+
+.esri-icon-basemap::before {
+  content: "\2913";
+}
+
+.esri-attribution__sources,
+.esri-component.esri-attribution.esri-widget {
+  display: none;
+}
 </style>
